353bc39f34c0a9ddc752626bf61696ba7bb9856c
Pritesh Lathia <plathia1998@gmail.com>
2022-03-29 23:30:39 +0530

    Update settings.py

--- SwiftCv/settings.py
+++ SwiftCv/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,4 +116,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
